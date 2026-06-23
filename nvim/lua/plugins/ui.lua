eddf6cbddad20fb5075674d7803bd9f079990b19
theaayushpatel <aayushp9326@gmail.com>
2026-06-23 09:20:21 +0530

    updated nvim configs

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+      transparent = true,
+    },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "tokyonight",
+      },
+    },
+  },
+}

df0562cbd4fb5820a4558426e85a6b4d636985ff
theaayushpatel <aayushp9326@gmail.com>
2026-06-27 15:03:05 +0530

    update finder, few tweaks

--- hypr/scripts/finder.sh
+++ hypr/scripts/finder.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 # 1. Search both paths. Add '--hidden' if you want to see dotfiles.
-selection=$(fd --type f . "$HOME" "/mnt/volume" | rofi -dmenu -i -p "Search Files:")
+selection=$(fd . "$HOME" "/mnt/volume" | rofi -dmenu -i -p "Search Files:")
 
 
 # 2. Open the selection (file or folder) directly
 if [[ -n "$selection" ]]; then
-    xdg-open "$selection"
+    if [[ -d "$selection" ]]; then
+        dolphin "$selection" &
+    else
+        xdg-open "$selection" &
+    fi
 fi

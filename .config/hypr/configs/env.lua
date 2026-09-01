d642bb6019f976f6636a09c4dd763bf87b01ad7b
theaayushpatel <aayushp9326@gmail.com>
2026-09-01 16:51:02 +0530

    updated kvantum and qt themes

--- .config/hypr/configs/env.lua
+++ .config/hypr/configs/env.lua
@@ -3,8 +3,9 @@
 -------------------------------
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
+hl.env("XCURSOR_THEME", "Adwaita")
 hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "Adwaita")
 hl.env("HYPRCURSOR_SIZE", "24")
--- hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
--- hl.env("QT_STYLE_OVERRIDE", "kvantum")
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_STYLE_OVERRIDE", "kvantum")
